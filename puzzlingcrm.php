1cca7dc73f11e57f15a7ee77e59aef00643ea2ae
Arsalan Arghavan <arsalanarghavan2014@gmail.com>
2025-09-29 17:23:56 +0330

    fixing bugs

--- puzzlingcrm.php
+++ puzzlingcrm.php
@@ -3,7 +3,7 @@
  * Plugin Name:       PuzzlingCRM
  * Plugin URI:        https://Puzzlingco.com/
  * Description:       A complete CRM and Project Management solution for Social Marketing agencies.
- * Version:           0.0.153
+ * Version:           0.0.154
  * Author:            Arsalan Arghavan
  * Author URI:        https://ArsalanArghavan.ir/
  * License:           GPL v2 or later
